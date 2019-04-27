232efa5218d35881ead9481eff0f15c48c589ccf
MingrenChen <chenmr9769@gmail.com>
2019-04-26 21:54:21 -0400

    A

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ var serv = require('http').Server(app);
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header('Access-Control-Allow-Methods', 'POST, GET');
-    // res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-    // res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
