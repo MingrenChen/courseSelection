0237203361ba58314fce0bb349b52f418784f512
MingrenChen <chenmr9769@gmail.com>
2019-04-26 22:05:18 -0400

    A

--- app.js
+++ app.js
@@ -20,8 +20,9 @@ var express = require('express');
 var app = express();
 
 app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
