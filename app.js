e03b665464bffae3aa131f08e3534289f322bfbd
MingrenChen <chenmr9769@gmail.com>
2019-04-26 20:55:38 -0400

    Allow cors

--- app.js
+++ app.js
@@ -21,15 +21,11 @@ var app = express();
 var serv = require('http').Server(app);
 
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-});
-
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", 'https//localhost:8080');
+    res.header('Access-Control-Allow-Methods', 'POST, GET');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
